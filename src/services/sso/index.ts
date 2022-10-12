103051db06d17dce56d0ef74769e629d049db6c4
Kevin Lucas <kevinluc08@gmail.com>
2022-10-12 13:18:19 +0000

    Try making sso into a module

---
+++ src/services/sso/index.ts
@@ -0,0 +1,6 @@
+export * from './constructs/assignment';
+export * from './constructs/group';
+export * from './constructs/instance-access-control-attribute-configuration';
+export * from './constructs/instance';
+export * from './constructs/permission-set';
+export * from './constructs/user';
