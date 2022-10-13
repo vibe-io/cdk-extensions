2be900dc4c8f38c1b678a1959dc3be471d9cd682
Kevin Lucas <kevinluc08@gmail.com>
2022-10-13 17:45:45 +0000

    Add codeartifact:PublishPackageVersion to CodeBuild service role

--- bin/init-aws.sh
+++ bin/init-aws.sh
@@ -5,18 +5,24 @@ if ! type aws &>/dev/null; then
     exit 1
 fi
 
-SCRIPT_PATH="$(dirname "$(readlink -f "${0}")")"
-TEMPLATE_PATH="${SCRIPT_PATH}/lib/template.yaml"
+TEMPLATE_NAME='ci-infrastructure.yaml'
+TEMPLATE_PATH='../cloudformation/templates'
+PARAMETERS_PATH='../cloudformation/parameters'
 
-if [[ -f "${SCRIPT_PATH}" ]]; then
-    echo "ERROR: Failed to find expected template file at '${SCRIPT_PATH}'." 1>&2
+SCRIPT_PATH="$(dirname "$(readlink -f "${0}")")"
+TEMPLATE_FILE="$(readlink -f "${SCRIPT_PATH}/${TEMPLATE_PATH}/${TEMPLATE_NAME}")"
+PARAMETERS_FILE="$(readlink -f "${SCRIPT_PATH}/${PARAMETERS_PATH}/${TEMPLATE_NAME%.*}.json")"
+
+if [[ ! -f "${TEMPLATE_FILE}" ]]; then
+    echo "ERROR: Failed to find expected template file at '${TEMPLATE_FILE}'." 1>&2
     exit 2
 fi
 
-echo "Using template at '${TEMPLATE_PATH}'."
+echo "Using template at '${TEMPLATE_FILE}'."
 
 aws cloudformation deploy \
     --capabilities CAPABILITY_IAM \
     --no-fail-on-empty-changeset \
-    --template-file "${TEMPLATE_PATH}" \
+    --parameter-overrides "$(test -f "${PARAMETERS_FILE}" && echo "file://${PARAMETERS_FILE}" || echo '[]')" \
+    --template-file "${TEMPLATE_FILE}" \
     --stack-name cdk-extensions
