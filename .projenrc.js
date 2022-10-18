42bb5de708ac74bd05e080b358b61a0a9e629908
Kevin Lucas <kevinluc08@gmail.com>
2022-10-18 14:11:36 +0000

    Remove more references to the old organization

--- .projenrc.js
+++ .projenrc.js
@@ -26,7 +26,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     module: projectName,
   },
   name: projectName,
-  repositoryUrl: 'https://github.com/RightBrain-Networks/cdk-extensions.git',
+  repositoryUrl: 'https://github.com/vibe-io/cdk-extensions.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
