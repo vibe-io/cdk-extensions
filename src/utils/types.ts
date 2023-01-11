072ec69ec951f3ab3d44446f1033209d0b9b5dea
Kevin Lucas <kevinluc08@gmail.com>
2023-01-11 04:27:46 +0000

    Add rds proxy endpoint L2 resource

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,9 @@
+import { Connections, IConnectable } from 'aws-cdk-lib/aws-ec2';
+
 export const isArray = (val: unknown): val is any[] => {
   return Object.prototype.toString.call(val) === '[object Array]';
+};
+
+export const isConnectable = (obj: unknown): obj is IConnectable => {
+  return (obj as any).connections instanceof Connections;
 };
