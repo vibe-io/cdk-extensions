cf668a8ce34cfa649eee4bafeb0511651d42e8c6
Kevin Lucas <Pharrox@users.noreply.github.com>
2023-06-19 05:00:43 +0000

    fix: Add required missing export

--- src/alerting/index.ts
+++ src/alerting/index.ts
@@ -3,5 +3,6 @@ export * from './issue-parsers';
 export * from './lib';
 
 export * from './issue-handler-override';
+export * from './issue-plugin-base';
 export * from './issue-manager';
 export * from './issue-trigger';
