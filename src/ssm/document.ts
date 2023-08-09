7ac69ee85fef0573dcf09fdc5375cfcfd5e43983
Kevin Lucas <Pharrox@users.noreply.github.com>
2023-08-09 20:16:56 +0000

    fix: Remove naming conflict with dotnet

--- src/ssm/document.ts
+++ src/ssm/document.ts
@@ -1,4 +1,3 @@
-import { ArnFormat } from 'aws-cdk-lib';
 import { IConstruct } from 'constructs';
 import { DocumentBase, DocumentBaseProps } from './document-base';
 
@@ -6,9 +5,6 @@ import { DocumentBase, DocumentBaseProps } from './document-base';
 export interface DocumentProps extends DocumentBaseProps {}
 
 export class Document extends DocumentBase {
-  public static readonly ARN_FORMAT: ArnFormat = ArnFormat.SLASH_RESOURCE_NAME;
-
-
   public constructor(scope: IConstruct, id: string, props: DocumentProps) {
     super(scope, id, props);
   }
