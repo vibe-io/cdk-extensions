a827b9c51d71c889706b3a623e5ae2d316a2764a
Kevin Lucas <Pharrox@users.noreply.github.com>
2023-10-26 03:56:15 +0000

    fix: Add missing exports

--- src/management/index.ts
+++ src/management/index.ts
@@ -2,5 +2,7 @@ export * from './lib';
 
 export * from './start-auto-scaling-group';
 export * from './start-ec2-instance';
+export * from './start-ecs-service';
 export * from './stop-auto-scaling-group';
 export * from './stop-ec2-instance';
+export * from './stop-ecs-service';
